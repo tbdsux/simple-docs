5e66f6926533b1ca5148786a9c4399640d2acd50
TheBoringDude <37932956+TheBoringDude@users.noreply.github.com>
2021-08-01 23:55:43 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
