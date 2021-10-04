86b857c43d02d5a523e07521248bcf457effce20
TheBoringDude <iamcoderx@gmail.com>
2021-10-05 00:13:40 +0800

    simple index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
+    fontFamily: {
+      sans: ['"Fira Sans"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   variants: {
